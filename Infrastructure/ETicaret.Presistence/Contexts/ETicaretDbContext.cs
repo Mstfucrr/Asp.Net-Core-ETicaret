d2577c8ea15197794cbee4f43c59374500cb6e7e
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-01-18 19:52:01 +0300

    ETicaretDbContext fix

--- Infrastructure/ETicaret.Presistence/Contexts/ETicaretDbContext.cs
+++ Infrastructure/ETicaret.Presistence/Contexts/ETicaretDbContext.cs
@@ -5,7 +5,21 @@ namespace ETicaret.Presistence.Contexts
     public class ETicaretDbContext : DbContext
     {
         public ETicaretDbContext(DbContextOptions<ETicaretDbContext> options) : base(options)
-        { }
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>(category =>
+            {
+                category.HasMany(c => c.ChildCategories)
+                    .WithOne(c => c.ParentCategory)
+                    .HasForeignKey(c => c.ParentCategoryId);
+            });
+        }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
@@ -13,8 +27,9 @@ namespace ETicaret.Presistence.Contexts
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
 
-        
+
 
     }
 }
