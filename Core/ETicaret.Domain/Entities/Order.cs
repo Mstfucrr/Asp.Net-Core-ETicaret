4d7fc65d0710b58100377bdd44d2e629c9205363
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-02-11 00:11:38 +0300

    add desc to order model

--- Core/ETicaret.Domain/Entities/Order.cs
+++ Core/ETicaret.Domain/Entities/Order.cs
@@ -5,9 +5,12 @@ namespace ETicaret.Domain.Entities
     public class Order : BaseEntity
     {
         
-        public string OrderNumber { get; set; }
-        public Customer Customer { get; set; }
-        public virtual ICollection<OrderDetail> OrderItems { get; set; }
+        public string? OrderNumber { get; set; }
+        public Guid? CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+        public virtual ICollection<OrderDetail>? OrderItems { get; set; }
         public decimal TotalPrice { get; set; }
+        public string? Description { get; set; }
+
     }
 }
